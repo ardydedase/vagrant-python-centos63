2c25b94a0834229885dbc395ee33ede62cb717cc
Ardy Dedase <ardy.dedase@gmail.com>
2013-07-24 11:23:23 +0100

    Use dos2unix

--- files/bin/virtualenv-auto-activate.sh
+++ files/bin/virtualenv-auto-activate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # virtualenv-auto-activate.sh
 # 
 # Installation:
@@ -24,12 +24,13 @@
 #       └── lib
 #
 #   The virtualenv will be activated automatically when you enter the directory.
+
 _virtualenv_auto_activate() {
     if [ -e ".venv" ]; then
         # Check to see if already activated to avoid redundant activating
         if [ "$VIRTUAL_ENV" != "$(pwd -P)/.venv" ]; then
             _VENV_NAME=$(basename `pwd`)
-            echo Activating virtualenv \"$_VENV_NAME\"...
+            echo "Activating virtualenv" \"$_VENV_NAME\"...
             VIRTUAL_ENV_DISABLE_PROMPT=1
             source .venv/bin/activate
             _OLD_VIRTUAL_PS1="$PS1"
@@ -38,5 +39,5 @@ _virtualenv_auto_activate() {
         fi
     fi
 }
- 
+
 export PROMPT_COMMAND=_virtualenv_auto_activate
